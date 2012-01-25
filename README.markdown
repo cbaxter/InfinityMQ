787d4a4a1b85e1711c8c1235c73c739c72dc342b
Chris Baxter <Chris.Baxter@shaw.ca>
2012-01-25 12:06:29 -0700

    Created empty solution structure and initial build process.

---
+++ README.markdown
@@ -0,0 +1,11 @@
+# InfinityMQ #
+
+## Development Environment ##
+- Visual Studio 2010
+  - Indentation: 2 spaces
+- Run `build.cmd` before opening solution for the first time.
+- Use [NuGet][nuget] for dependencies (where available)
+  - [NuGet Package Manager][nuget-pm]
+  
+[nuget]: http://nuget.org/
+[nuget-pm]: http://visualstudiogallery.msdn.microsoft.com/27077b70-9dad-4c64-adcf-c7cf6bc9970c
---
+++ src/Shared/ProductInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCompany("InfinityMQ")]
+[assembly: AssemblyProduct("InfinityMQ")]
+[assembly: AssemblyCopyright("Copyright © 2012 InfinityMQ")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
