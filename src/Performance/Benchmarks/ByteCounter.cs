7ce17f6c2a9a8d40267741f267322e9446c27b83
Chris Baxter <Chris.Baxter@shaw.ca>
2012-01-27 19:59:07 -0700

    Added missing files from prior commit.

---
+++ src/Performance/Benchmarks/ByteCounter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace InfinityMQ.Performance.Benchmarks
+{
+    internal class ByteCounter
+    {
+        private readonly Int64 expectedBytes;
+        private Int64 clientBytesReceived;
+        private Int64 serverBytesReceived;
+        private Int64 clientBytesSent;
+        private Int64 serverBytesSent;
+
+        public Boolean AllClientBytesSent { get { return Interlocked.Read(ref this.clientBytesSent) == expectedBytes; } }
+        public Boolean AllServerBytesSent { get { return Interlocked.Read(ref this.serverBytesSent) == expectedBytes; } }
+        public Boolean AllClientBytesReceived { get { return Interlocked.Read(ref this.clientBytesReceived) == expectedBytes; } }
+        public Boolean AllServerBytesReceived { get { return Interlocked.Read(ref this.serverBytesReceived) == expectedBytes; } }
+
+        public ByteCounter(Int64 expectedBytes)
+        {
+            this.expectedBytes = expectedBytes;
+        }
+
+        public void CaptureClientBytesReceived(Int64 bytesReceived)
+        {
+            Interlocked.Add(ref this.clientBytesReceived, bytesReceived);
+        }
+
+        public void CaptureClientBytesSent(Int64 bytesSent)
+        {
+            Interlocked.Add(ref this.clientBytesSent, bytesSent);
+        }
+
+        public void CaptureServerBytesReceived(Int64 bytesReceived)
+        {
+            Interlocked.Add(ref this.serverBytesReceived, bytesReceived);
+        }
+
+        public void CaptureServerBytesSent(Int64 bytesSent)
+        {
+            Interlocked.Add(ref this.serverBytesSent, bytesSent);
+        }
+    }
+}
---
+++ src/Performance/Benchmarks/Metrics.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace InfinityMQ.Performance.Benchmarks
+{
+    internal class Metrics
+    {
+        public Int32 MessageCount { get; private set; }
+        public Int32 MessageSize { get; private set; }
+        public Decimal DataThroughput { get; private set; }
+        public Decimal MessageThroughput { get; private set; }
+        public Decimal MessageLatency { get; private set; }
+
+        public Metrics(Int32 messageCount, Int32 messageSize)
+        {
+            MessageCount = messageCount;
+            MessageSize = messageSize;
+        }
+
+        public void CalculateLatency(Int64 elapsedTicks)
+        {
+            MessageLatency = elapsedTicks / (Decimal)MessageCount / 2M * 1000000M / Stopwatch.Frequency;
+        }
+
+        public void CalculateThroughput(Int64 elapsedTicks)
+        {
+            MessageThroughput = (Decimal)MessageCount * Stopwatch.Frequency / elapsedTicks;
+            DataThroughput = MessageThroughput * MessageSize * 8M / 1000000M;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Message Latency = {0} [us]; Message Throughput = {1} [msg/s]; Data Throughput = {2} [Mb/s];",
+                       MessageLatency.ToString("F2").PadLeft(10, ' '),
+                       MessageThroughput.ToString("F2").PadLeft(10, ' '),
+                       DataThroughput.ToString("F2").PadLeft(10, ' ')
+                   );
+        }
+    }
+}
